46398a9fb82bbf7619aa0cc6fd5e3c237106cb39
Arthur Henrique <arthurhenrique@users.noreply.github.com>
2018-11-23 01:12:55 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from bottle import route, run, request
 
-@route('/hello')
-def hello():
-    x = request.environ.get('HTTP_X_FORWARDED_FOR') or request.environ.get('REMOTE_ADDR')
+@route('/')
+def get_address():
+    """ return the forwarded address """
+    req = request.environ.get('HTTP_X_FORWARDED_FOR') or request.environ.get('REMOTE_ADDR')
 
-    return "Hello World!" +  "EAE" + str(x)
+    return "address: {}".format(req)
 
 run(host='0.0.0.0', port=8080, debug=True)
